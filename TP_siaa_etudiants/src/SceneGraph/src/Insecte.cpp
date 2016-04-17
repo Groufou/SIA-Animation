e381c528a1bf06a389a126cd48b2d7a973fad2cb
Foxshiver <vincent.goupil@hotmail.com>
2016-04-17 19:26:10 +0200

    3ds body

--- TP_siaa_etudiants/src/SceneGraph/src/Insecte.cpp
+++ TP_siaa_etudiants/src/SceneGraph/src/Insecte.cpp
@@ -18,7 +18,8 @@ SceneGraph::Insecte::Insecte(float speed) :
 	
 	m_body = new Sphere(matBody);
 	m_scaleBody = new Scale(Math::makeVector(1.0f, 0.3f, 0.3f));
-
+	//m_body3ds = new HelperGl::Loader3ds("../../INSECTE/Insecte.3DS","../../INSECTE/textures");
+	//m_bodyMesh = new MeshVBO_v2(m_body3ds->getMeshes()[0]);
 
 	// Insect Wings
 	HelperGl::Material matWing;
@@ -61,6 +62,8 @@ SceneGraph::Insecte::~Insecte(void)
 {
 	delete m_body;
 	delete m_scaleBody;
+	delete m_body3ds;
+	delete m_bodyMesh;
 
 	delete m_wing;
 	delete m_scaleWing;
@@ -90,7 +93,7 @@ void SceneGraph::Insecte::createSkeleton(void)
 
 	// Insect Body
 	m_scaleBody->addSon(m_body);
-
+	//m_scaleBody->addSon(m_bodyMesh);
 	// Insect Wings
 	m_translateAxeWingR->addSon(m_rotateWingR);
 	m_translateAxeWingL->addSon(m_rotateWingL);
